a5c42c1b41cad4f2938bc88ee39660dd2b3cea8b
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-04-25 22:03:49 -0700

    Time: 70 ms (47.44%), Space: 98.6 MB (29.65%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>s;
+        for(int i=0;i<nums.size();i++){
+            if(s.count(nums[i])){
+                if(i-s[nums[i]]<=k){
+                    return true;
+                }
+            }
+            s[nums[i]]=i;
+        }
+        return false;
+    }
+};
