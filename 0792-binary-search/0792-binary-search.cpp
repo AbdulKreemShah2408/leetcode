6816619088ec77a861e19900a87a10fdeedc6333
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-08-06 21:59:59 -0700

    Time: 0 ms (100%), Space: 31.3 MB (79.78%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int start=0, end=n-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+       int n=nums.size();
+       int left=0, right=n-1;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]>target){
+            right=mid-1;
+        }else{
+        left=mid+1;
+        }
+    }
+    return -1;
     }
 };
