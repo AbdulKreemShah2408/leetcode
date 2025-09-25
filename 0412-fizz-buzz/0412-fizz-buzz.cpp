933d49ab773ac60cc13d70db8d6192df7bca7a80
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-09-25 12:36:59 -0700

    Time: 1 ms (53.94%), Space: 11.8 MB (43.65%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,18 +3,19 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string>ans;
         for(int i=1;i<=n;i++){
-            if( i%3==0 && i%5==0){
-                ans.push_back("FizzBuzz");
+            if(i%3==0 && i%5==0){
+              ans.push_back("FizzBuzz");
             }
-            else if(i%3==0){
+            else if (i%3==0){
                 ans.push_back("Fizz");
             }
-            else if(i%5==0){
-                ans.push_back("Buzz");
-            }
-            else{
-                ans.push_back(to_string(i));
-            }
+                else if(i%5==0){
+                   ans.push_back("Buzz");
+                }
+                else{
+                    ans.push_back(to_string(i));
+                }
+            
         }
         return ans;
     }
