1ad186e4db04671123b9291ace431370a74e9814
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-07 00:41:52 -0700

    Time: 23 ms (6.71%), Space: 27.9 MB (5%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-         int start=0, end=n-1;
-         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==mid){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-
-         }
-         return start;
-
+       unordered_map<int,bool>m;
+        for(int num:nums){
+            m[num]=true;
+        }
+        for(int i=0;i<=n;i++){
+        if(m.find(i) == m.end()){
+            return i;
+        }
+        }
+        return -1;
     }
 };
