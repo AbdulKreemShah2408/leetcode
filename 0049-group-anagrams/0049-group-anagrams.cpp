09155bd32897a56d4de32cacf47a99d77d6ecd25
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-08 04:09:48 -0700

    Time: 19 ms (48.31%), Space: 26.2 MB (38.65%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
         unordered_map<string,vector<string>>m;
-        for(auto it :strs){
-            string temp=it;
-            sort(it.begin(),it.end());
-            m[it].push_back(temp);
-
+        for(auto it:strs){
+          string temp=it;
+          sort(it.begin(),it.end());
+          m[it].push_back(temp);
+         
         }
         vector<vector<string>>ans;
         for(auto x:m){
-         ans.push_back(x.second);
+            ans.push_back(x.second);
         }
         return ans;
     }
