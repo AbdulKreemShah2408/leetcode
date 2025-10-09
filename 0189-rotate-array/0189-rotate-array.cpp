04f401e702caca41d3e40a5c8fef4164cbbabc46
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-09 01:33:50 -0700

    Time: 0 ms (100%), Space: 29.5 MB (83.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,9 +3,23 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-
+        int left=0, right=n-1;
+        while(left<right){
+            swap(nums[left],nums[right]);
+            left++;
+            right--;
+        }
+         left=0, right=k-1;
+        while(left<right){
+        swap(nums[left],nums[right]);
+        left++;
+        right--;
+        }
+         left=k, right=n-1;
+        while(left<right){
+            swap(nums[left],nums[right]);
+            left++;
+            right--;
+        }
     }
 };
