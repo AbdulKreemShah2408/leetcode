410139c27b9b694d15248cb2bcc745d9b9303c68
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-09 05:23:24 -0700

    Time: 0 ms (100%), Space: 8.2 MB (99.84%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-      int left=0, right=0;
-      string ans;
-      while(left<word1.size() && right<word2.size()){
+     int left=0, right=0;
+     string ans;
+     while(left<word1.size() && right<word2.size()){
         ans.push_back(word1[left]);
         ans.push_back(word2[right]);
         left++;
         right++;
-      }
-      while(left<word1.size()){
+     }
+     while(left<word1.size()){
         ans.push_back(word1[left]);
         left++;
-      }
-      while(right<word2.size()){
+     }
+     while(right<word2.size()){
         ans.push_back(word2[right]);
         right++;
-      }
-      return ans;
+     }
+     return ans;
     }
 };
