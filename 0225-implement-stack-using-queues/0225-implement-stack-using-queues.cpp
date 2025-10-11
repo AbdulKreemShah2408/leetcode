2335a48721b494c2a40d7f0d22eefabd71ab5cfa
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-11 11:54:07 -0700

    Time: 0 ms (100%), Space: 9.6 MB (18.72%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -2,26 +2,28 @@ class MyStack {
 public:
   queue<int>q1;
   queue<int>q2;
+
     MyStack() {
         
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
+       while(!q1.empty()){
+        q2.push(q1.front());
+        q1.pop();
+       }
+       q1.push(x);
+       while(!q2.empty()){
+        q1.push(q2.front());
+        q2.pop();
+       }
+       
     }
     
     int pop() {
-        int ans=q1.front();
-        q1.pop();
-        return ans;
+     int ans=q1.front();
+     q1.pop();
+     return ans;
     }
     
     int top() {
