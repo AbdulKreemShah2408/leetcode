f81685b3e22315ba1beac400ae247d52e28086ca
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2025-10-11 07:13:31 -0700

    Time: 4 ms (68.29%), Space: 14.2 MB (36.26%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>s1;
+        for(char c:s){
+            if(!s1.empty() && s1.top()==c){
+                s1.pop();
+            }
+            else{
+                s1.push(c);
+            }
+        }
+        string result;
+        while(!s1.empty()){
+            result +=s1.top();
+            s1.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
