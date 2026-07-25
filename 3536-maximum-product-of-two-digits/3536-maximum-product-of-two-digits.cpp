1f5a1715e9ec8c5ff7dfb4362e0cd3cd56c5d55c
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-07-25 09:05:30 +0500

    Time: 3 ms (15.04%), Space: 8.9 MB (54.99%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+       vector<int>digits;
+       while(n>0){
+        digits.push_back(n%10);
+        n /=10;
+       }
+       int max_prod=0;
+       int size=digits.size();
+       for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            int current_prod=digits[i]*digits[j];
+            max_prod=max(max_prod,current_prod);
+        }
+       }
+       return max_prod;
+    }
+};
